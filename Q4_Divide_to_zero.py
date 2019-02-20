9bb9449e3a8540e8db1238089cf431e8b9b46927
Clauric <47009424+Clauric@users.noreply.github.com>
2019-02-20 20:34:07 +0000

    Update Q4_Divide_to_zero.py

--- Q4_Divide_to_zero.py
+++ Q4_Divide_to_zero.py
@@ -1,9 +1,11 @@
-# Python assignment Question 4:
+# Python assignment Question 4
 # Student Name: Mark Biggar
 # Student Number: G00376334
 
 # Write a program that asks the user to input any positive integer and outputs the successive values of the following calculation. At each step calculate the next value
 # by taking the current value and, if it is even, divide it by two, but if it is odd, multiply it by three and add one. Have the program end if the current value is one.
+
+# Note: This problem is commonly referred to as the Collatz Conjecture
 
 # Approach:
 # Get input value from user
